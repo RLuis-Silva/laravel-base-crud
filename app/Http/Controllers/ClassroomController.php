44ce3f0b943f27000c7c7c905a53b11744ac0576
Luis <s.ros.lui@gmail.com>
2021-01-31 08:25:11 +0100

    add rotta classrooms.index (metodo 'index()' del ClassroomController)

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -13,7 +13,7 @@ class ClassroomController extends Controller
      */
     public function index()
     {
-        //
+        return 'classrooms index page';
     }
 
     /**
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="{{ route('homepage') }}">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="{{ route('homepage') }}">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="{{ route('classrooms.index') }}">Classrooms</a>
             </li>
           </ul>
         </div>
